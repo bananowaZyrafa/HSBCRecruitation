bdd3f7f01540e95532b838ed301d7eab2215645f
BartoszWolinski <>
2020-08-17 15:31:55 +0200

    initial commit

---
+++ HSBCCities/HSBCCities/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HSBCCities
+//
+//  Created by Paweł W. on 17/08/2020.
+//  Copyright © 2020 Bartosz.Wolinski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
