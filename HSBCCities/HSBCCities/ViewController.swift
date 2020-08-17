7e1340c6c212dbdbf19df0f9672125664602f950
BartoszWolinski <wally93@gmail.com>
2020-08-17 15:51:29 +0200

    add networking scheme

--- HSBCCities/HSBCCities/ViewController.swift
+++ HSBCCities/HSBCCities/ViewController.swift
@@ -1,20 +1,6 @@
-//
-//  ViewController.swift
-//  HSBCCities
-//
-//  Created by Paweł W. on 17/08/2020.
-//  Copyright © 2020 Bartosz.Wolinski. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
 
 }
 
